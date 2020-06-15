c745c08312be3f2cea2cb5180011134570b47da3
sszza120 <z875ab@gmail.com>
2020-06-15 11:17:54 +0800

    專案已完成

--- app/src/main/java/com/sszza120/myqrcodeapp/MainActivity.java
+++ app/src/main/java/com/sszza120/myqrcodeapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.sszza120.myqrcodeapp;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtInputText = findViewById(R.id.edtInputText);
+        imgQRcode = findViewById(R.id.imgQRcode);
+        btnQRcode = findViewById(R.id.btnQRcode);
+
+        btnQRcode.setOnClickListener(btn_qrcode);
+    }
+    private Button.OnClickListener btn_qrcode = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            getCode(view);
+        }
+    };
+    public void getCode(View view){
+        BarcodeEncoder encoder = new BarcodeEncoder();
+        try {
+            Bitmap bit = encoder.encodeBitmap(edtInputText.getText().toString(),
+                    BarcodeFormat.QR_CODE,
+                    250,250);
+                imgQRcode.setImageBitmap(bit);
+        }catch (WriterException e){
+            e.printStackTrace();
+        }
 
     }
+
 }

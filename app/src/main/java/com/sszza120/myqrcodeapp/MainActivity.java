a857c083e7de4b4d8db71d694716888c0d492653
sszza120 <z875ab@gmail.com>
2020-06-15 10:14:29 +0800

    布局已完成

--- app/src/main/java/com/sszza120/myqrcodeapp/MainActivity.java
+++ app/src/main/java/com/sszza120/myqrcodeapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.sszza120.myqrcodeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtInputText;
+    private ImageView imgQRcode;
+    private Button btnQRcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
